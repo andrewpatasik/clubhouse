75127669f0f5fda598e3331b29a6d5a210201f40
Andrew Patasik <andrew.yehezkiel17@gmail.com>
2022-03-01 18:52:56 +0800

    code: login and signup view added

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,12 @@ router.get('/', (req, res) => {
   })
 })
 
+router.get('/signup', (req, res) => {
+  res.render('signup')
+})
+
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
 module.exports = router;
