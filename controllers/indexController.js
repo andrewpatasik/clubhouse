00a2080a8776c5fa3a8d3d772cf9f50a6b34e94c
Andrew Patasik <andrew.yehezkiel17@gmail.com>
2022-05-17 17:15:51 +0800

    styling front page

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,53 +1,72 @@
-const express = require('express');
-const passport = require('passport');
-const bcrypt = require('bcryptjs') ;
+const express = require("express");
+const passport = require("passport");
+const bcrypt = require("bcryptjs");
+const async = require("async");
 
-const User = require('../models/user');
-const Post = require('../models/post');
+const User = require("../models/user");
+const Post = require("../models/post");
 
 exports.index = (req, res, next) => {
   if (!req.isAuthenticated()) {
-    Post.find({})
-      .populate('user')
-      .exec((err, result) => {
+    // fetch posts and users
+    async.parallel({
+      posts: function (callback) {
+        Post.find({}).populate("user").exec((callback))
+      },
+      users: function (callback) {
+        User.find({}).exec((callback))
+      }},
+      function(err, results) {
         if (err) return next(err)
 
-        res.render('index', {
-          title: "Share Your Story To Be Heard | Overheard 🧠 💡",
+        res.render("index", {
+          title: "Share Your Story To Be Heard | Klubhaus 🧠 💡",
           user: req.user,
-          feed: result
-        })
-      })
+          feed: results.posts,
+          member: results.users,
+          error: err
+        });
+    });
+    // Post.find({})
+    //   .populate("user")
+    //   .exec((err, result) => {
+    //     if (err) return next(err);
+    //     res.render("index", {
+    //       title: "Share Your Story To Be Heard | Klubhaus 🧠 💡",
+    //       user: req.user,
+    //       feed: result,
+    //     });
+    //   });
   } else {
-    res.redirect('/feed');
+    res.redirect("/feed");
   }
 };
 
 exports.login_get = (req, res, next) => {
-  res.render('login', {
-    layout: '../views/layouts/sign-form',
-    title: 'Log In',
-    pageName: 'login'
-  })
-}
+  res.render("login", {
+    layout: "../views/layouts/sign-form",
+    title: "Log In",
+    pageName: "login",
+  });
+};
 
-exports.login_post = passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/login'
-})
+exports.login_post = passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/login",
+});
 
 exports.logout_post = (req, res) => {
   req.logout();
-  res.redirect('/')
-}
+  res.redirect("/");
+};
 
 exports.signup_post = (req, res, next) => {
   User.findOne({ username: req.body.username }, (err, user) => {
     if (err) return next(err);
 
     if (user) {
-      console.log(user.username + ' is already existed')
-      res.redirect('/')
+      console.log(user.username + " is already existed");
+      res.redirect("/");
     } else {
       bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
         if (err) return next(err);
@@ -55,15 +74,15 @@ exports.signup_post = (req, res, next) => {
         let user = new User({
           name: req.body.fullname,
           username: req.body.username,
-          password: hashedPassword
+          password: hashedPassword,
         });
 
         user.save((err) => {
           if (err) return next(err);
-          console.log('successfully created');
+          console.log("successfully created");
           next();
-        })
-      })
+        });
+      });
     }
-  })
-}
+  });
+};
